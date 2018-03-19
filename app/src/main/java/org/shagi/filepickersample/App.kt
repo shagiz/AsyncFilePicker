8f2506dd2a7cb73a80dcc38b0e5b7df8a7263f62
shagi <gpetropavlov@mons.site>
2018-03-19 17:23:40 +0300

    version 0.1

---
+++ app/src/main/java/org/shagi/filepickersample/App.kt
@@ -0,0 +1,12 @@
+package org.shagi.filepickersample
+
+import android.app.Application
+
+import timber.log.Timber
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
